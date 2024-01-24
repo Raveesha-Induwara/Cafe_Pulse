cbca09774717a035540ad1dabd4742a8cf25faad
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-01-24 21:43:57 +0530

    initial commit

---
+++ src/components/CustomIcon.ts
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import icoMoonConfig from '../../selection.json';
+export default createIconSetFromIcoMoon(icoMoonConfig);
