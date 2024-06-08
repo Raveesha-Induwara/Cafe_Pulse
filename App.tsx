13582c81c92b1d06dcfc1373ca442b77607f3a8e
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-06-08 18:13:20 +0530

    Added App icon and Splash screen images

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, {useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TabNavigator from './src/navigators/TabNavigator';
 import {DetailsScreen} from './src/screens/DetailsScreen';
-import PaymentScreen from './src/screens/PaymentScreen';
+import {PaymentScreen} from './src/screens/PaymentScreen';
+import SplashScreen from 'react-native-splash-screen';
 
 const stack = createNativeStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
-      <stack.Navigator screenOptions={{headerShown: false}}>
+      <stack.Navigator
+        initialRouteName="Tab"
+        screenOptions={{headerShown: false}}>
         <stack.Screen
           name="Tab"
           component={TabNavigator}
