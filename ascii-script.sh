7cb5cd231201d5c7bdad8534bad5df3ccdf80dcb
Raveesha Induwara Abeysinghe <95018494+Raveesha-Induwara@users.noreply.github.com>
2025-09-14 20:51:48 +0530

    Added download and upload actions

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a DRAGON.....RAWR" >> dragon.txt
-grep -i "dragon" dragon.txt
-cat dragon.txt
-ls -ltra
+# grep -i "dragon" dragon.txt
+# cat dragon.txt
+# ls -ltra
